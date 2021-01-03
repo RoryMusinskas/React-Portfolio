ef4e32484fca8444ea1dc54c66f649736f055916
RoryMusinskas <devrorym@gmail.com>
2021-01-03 19:16:45 +1100

    Init of the App file, rendering all the basic components of the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { render } from "@testing-library/react";
+import Header from "./components/Header";
+import AboutArticle from './components/About-article'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <!-- header component with links to the 4 main pages on my website --> */}
+      <Header />
+      {/* <!-- main component which contains information about me. It also links to about me and contact me --> */}
+      <AboutArticle />
+      {/* <!-- skills component which contains my school/work history and skills--> */}
+      <Skills />
+      {/* <!-- projects component which shows off projects I have created-->*/}
+      <Projects />
+      {/* <!-- contact component for people to get in touch with me-->*/}
+      <Contact />
+      {/* <!-- footer container which has three icons that link to my social accounts --> */}
+      <Footer />
     </div>
   );
 }
