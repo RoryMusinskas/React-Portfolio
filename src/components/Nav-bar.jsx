4ed9741059928a4a3a59c9c32c5462280bcd36a8
RoryMusinskas <devrorym@gmail.com>
2021-01-03 19:19:40 +1100

    Basic implementation of the nav-bar componenent

---
+++ src/components/Nav-bar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="main-nav">
+      <a href={'#home'}>Home</a>
+      <a href={"#about"}>About</a>
+      <a href={"#projects"}>Projects</a>
+      <a href="contact.html">Contact</a>
+    </nav>
+  );
+};
+
+export default Navbar 
