a33655924aa3e2cb0fe956faf4aaf45dc3f3a385
RoryMusinskas <devrorym@gmail.com>
2021-01-03 20:23:37 +1100

    Changed path for link to contact section

--- src/components/Nav-bar.jsx
+++ src/components/Nav-bar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <a href={'#home'}>Home</a>
       <a href={"#about"}>About</a>
       <a href={"#projects"}>Projects</a>
-      <a href="contact.html">Contact</a>
+      <a href={'#contact'}>Contact</a>
     </nav>
   );
 };
