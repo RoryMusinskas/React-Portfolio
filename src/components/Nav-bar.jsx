43c0dda5b171f2cc26f6449b8b65169617804cf5
RoryMusinskas <devrorym@gmail.com>
2021-01-04 19:51:41 +1100

    Updated nav bar to be responsive for phone

--- src/components/Nav-bar.jsx
+++ src/components/Nav-bar.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
+  const toggleHamburger = () => {
+    // target the nav ul element
+    const navUL = document.querySelector("#nav-ul");
+    // toggle the show class on click
+    navUL.classList.toggle("show");
+  };
+
   return (
     <nav className="main-nav">
-      <a href={'#home'}>Home</a>
-      <a href={"#about"}>About</a>
-      <a href={"#projects"}>Projects</a>
-      <a href={'#contact'}>Contact</a>
+      <button className="hamburger" id="hamburger" onClick={toggleHamburger}>
+        <i className="fas fa-bars fa-3x"></i>
+      </button>
+      <ul className="nav-ul" id="nav-ul">
+        <li>
+          <a href={"#home"}>Home</a>
+        </li>
+        <li>
+          <a href={"#about"}>About</a>
+        </li>
+        <li>
+          <a href={"#projects"}>Projects</a>
+        </li>
+        <li>
+          <a href={"#contact"}>Contact</a>
+        </li>
+      </ul>
     </nav>
   );
 };
 
-export default Navbar 
+export default Navbar;
+
